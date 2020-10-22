a1127ef96a6a272a56eb642be0328825f35a73f2
Egor Pozhitok <pozhitok.e@gmail.com>
2020-10-22 22:37:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,23 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    var encodedLetters = expr.match(/.{1,10}/g),
+        encodedString = '';
+        encodedLetters.forEach(function(value) {
+            if(value === '**********') {
+                encodedString += ' ';
+            } else {
+                var letters = value.match(/.{1,2}/g),
+                    morseString = '';
+                letters.forEach(function(val) {
+                if(val !== '00') {
+                    val === '10' ? morseString += '.' : morseString += '-';
+                }
+                })
+                encodedString += MORSE_TABLE[morseString];
+            }
+        })
+    return encodedString;
 }
 
 module.exports = {
